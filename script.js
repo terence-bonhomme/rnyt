ce0d26ae43905cc6be45c304296de2d908f2c19d
Glitch (local-maroon-badger) <none>
2021-12-08 20:06:24 +0000

    quick fix about the delay

--- script.js
+++ script.js
@@ -1991,7 +1991,6 @@ async function onYouTubePlayerAPIReady() {
       temp_chapter.id = "c" + position;
 
       var rem = await RemNoteAPI.v0.get(child_array[position]);
-
       const clock = rem.name[0].text;  
 
       temp_chapter.delay = formatedTimeToDuration(clock);
